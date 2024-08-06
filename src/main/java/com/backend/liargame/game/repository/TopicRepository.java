7ed701a446ccd617a2bf4d8a786f2e57548ab834
Changho0514 <chh9450@naver.com>
2024-08-06 19:50:46 +0900

    feat - 토픽, 키워드 리포지토리 생성

--- src/main/java/com/backend/liargame/game/repository/TopicRepository.java
+++ src/main/java/com/backend/liargame/game/repository/TopicRepository.java
@@ -2,6 +2,11 @@ package com.backend.liargame.game.repository;
 
 import com.backend.liargame.game.entity.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface TopicRepository extends JpaRepository<Topic, Long> {
+    @Query(value = "SELECT * FROM topic ORDER BY RAND() LIMIT 1", nativeQuery = true)
+    Optional<Topic> findRandomTopic();
 }
