3cb36e43828540084aa4bad0135fa69fdb2e9e6d
Changho0514 <chh9450@naver.com>
2024-08-10 19:46:00 +0900

    feat - 라이어의 마지막 기회에 선택지들(15개) 가져오기 위해서 쿼리문 작성

--- src/main/java/com/backend/liargame/game/repository/KeywordRepository.java
+++ src/main/java/com/backend/liargame/game/repository/KeywordRepository.java
@@ -4,9 +4,14 @@ import com.backend.liargame.game.entity.Keyword;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
     @Query(value = "SELECT * FROM keyword WHERE topic_id = ?1 ORDER BY RAND() LIMIT 1", nativeQuery = true)
     Optional<Keyword> findRandomKeywordByTopicId(Long topicId);
+
+    @Query(value = "SELECT keyword.name FROM keyword WHERE topic_id = ?1 AND keyword.name <> ?2 ORDER BY RAND() LIMIT 14", nativeQuery = true)
+    List<String> findOptionKeywordByTopicIdExcludingKeyword(Long topicId, String keyword);
+
 }
